1f8563ae722e3e79225e8b3061f619b18f98f42c
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2021-09-24 19:05:29 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -16,11 +16,11 @@ oru helpum nee ivadunnu pretheekshikkanda🤭😂🤦‍♂️
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/The_bad_boyii">The_bad_boyii</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://github.com/Lallu-lallus/Auto-filter-bot-v3">Click Me</a></i>
 """
